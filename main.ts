96f2401f5bc502e009b3ac12c74132536dde54d9
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2023-01-19 10:42:42 -0300

    finish first API version

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from './generated/client/deno/edge.js'
-import { Application, Router, RouterContext } from 'https://deno.land/x/oak@v11.1.0/mod.ts'
+import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts'
 import { config } from "https://deno.land/std@0.171.0/dotenv/mod.ts";
 
 const envVars = await config();
